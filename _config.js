6e1e5678c975438e0157cc2ea9ff62f846d74d7b
23po <ckodalo@gmail.com>
2023-09-19 14:33:40 +0300

    added mongodb string

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    //production: 'mongodb+srv://calvinkodalo:eSEht3QWnM8FwVNe@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    //development: 'mongodb+srv://calvinkodalo:eSEht3QWnM8FwVNe@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    //test: 'mongodb+srv://calvinkodalo:eSEht3QWnM8FwVNe@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://calvinkodalo:eSEht3QWnM8FwVNe@cluster0.8b4tjou.mongodb.net/?retryWrites=true&w=majority&appName=darkroom',
+    development: 'mongodb+srv://calvinkodalo:eSEht3QWnM8FwVNe@cluster0.8b4tjou.mongodb.net/?retryWrites=true&w=majority&appName=darkroom-dev',
+    test: 'mongodb+srv://calvinkodalo:eSEht3QWnM8FwVNe@cluster0.8b4tjou.mongodb.net/?retryWrites=true&w=majority&appName=darkroom-test'
+
 }
 module.exports = config;
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
+let mongodb_url = 'mongodb+srv://calvinkodalo:1234@cluster0.8b4tjou.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
+let dbName = 'Cluster0';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
 });
